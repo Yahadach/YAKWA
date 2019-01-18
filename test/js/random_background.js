4bf3236e0470e0f18df52c2dea98bfe6b004ef35
Yahadach <ahadachyouness@gmail.com>
2019-01-18 16:48:18 +0100

    header ok avec photo de films en fond

--- test/js/random_background.js
+++ test/js/random_background.js
@@ -3,7 +3,7 @@ headerImage = document.getElementById('header_background');
 
 function genBackground() {
 
-    var stockNb = Math.floor(Math.random() * Math.floor(6));
+    var stockNb = Math.floor(Math.random() * Math.floor(25));
     console.log(stockNb);
     headerImage.style.backgroundImage = "url('img/photos/" + stockNb + ".jpg')";
     headerImage.style.backgroundSize = "cover";
