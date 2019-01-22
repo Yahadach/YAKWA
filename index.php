6b8b699ee07bb92ae30f891ee4a278020964e23c
gelelf <coralie.leclerc@outlook.com>
2019-01-22 13:44:11 +0100

    htaccess

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-require_once 'vendor/autoload.php';
+<?php
+//Charger l'autoload
+require 'vendor/autoload.php';
 //Creer environnement TWIG
 $loader = new Twig_Loader_Filesystem('views');
 $twig = new Twig_Environment($loader);
